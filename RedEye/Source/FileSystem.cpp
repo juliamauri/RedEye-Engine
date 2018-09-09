27fb79e67d978e7dc3d9c2e5b9ab4df574e5b56d
Julià Mauri Costa <julimacowork@gmail.com>
2018-09-09 15:58:42 +0200

    :information_desk_person: Maybe memory leak solution

--- RedEye/Source/FileSystem.cpp
+++ RedEye/Source/FileSystem.cpp
@@ -185,6 +185,32 @@ unsigned int RE_FileIO::HardLoad()
 	return ret;
 }
 
+/* Maybe memory leak solution
+char* file_read(const char* filename) {
+		SDL_RWops *rw = SDL_RWFromFile(filename, "rb");
+		if (rw == NULL) return NULL;
+
+		Sint64 res_size = SDL_RWsize(rw);
+		char* res = (char*)malloc(res_size + 1);
+
+		Sint64 nb_read_total = 0, nb_read = 1;
+		char* buf = res;
+		while (nb_read_total < res_size && nb_read != 0) {
+				nb_read = SDL_RWread(rw, buf, 1, (res_size - nb_read_total));
+				nb_read_total += nb_read;
+				buf += nb_read;
+		}
+		SDL_RWclose(rw);
+		if (nb_read_total != res_size) {
+				free(res);
+				return NULL;
+		}
+
+		res[nb_read_total] = '\0';
+		return res;
+}
+*/
+
 ////////////////////////////////////////////////////////////////////
 ///////////////////Config
 ////////////////////////////////////////////////////////////////////
