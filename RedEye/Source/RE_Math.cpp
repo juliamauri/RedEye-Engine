6ef64e07575fe771ed73347b6c146f7d63e1aa30
Ruben Sardon <ec4who@hotmail.com>
2018-09-17 19:40:30 +0200

    Added RE_Math :performing_arts:
    Added random number generation
    Contained in App

---
+++ RedEye/Source/RE_Math.cpp
@@ -0,0 +1,43 @@
+#include "RE_Math.h"
+#include "MathGeoLib\include\Time\Clock.h"
+
+RE_Math::RE_Math()
+{
+	lcg.Seed(Clock::TickU32());
+}
+
+RE_Math::RE_Math(unsigned int seed)
+{
+	lcg.Seed(seed);
+}
+
+float RE_Math::RandomF()
+{
+	// range [ 0, 1 )
+	return lcg.Float();
+}
+
+int RE_Math::RandomInt()
+{
+	// range [ 0, 2147483647 ]
+	return lcg.Int();
+}
+
+float RE_Math::RandomF(float min, float max)
+{
+	// @param a Lower bound, inclusive.
+	// @param b Upper bound, inclusive.
+	return lcg.Float(min, max);
+}
+
+int RE_Math::RandomInt(int min, int max)
+{
+	// @param a Lower bound, inclusive.
+	// @param b Upper bound, exclusive.
+	return lcg.Int(min, max);
+}
+
+void RE_Math::SetRNGSeed(unsigned int seed)
+{
+	lcg.Seed(seed);
+}
