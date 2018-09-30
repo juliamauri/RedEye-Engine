754dcfd3784674603c4d57f536a21e77a4599948
Julià Mauri Costa <julimacowork@gmail.com>
2018-09-30 15:49:28 +0200

    Added Component Mesh  :up:
    +mesh loaded with assimp

--- RedEye/Source/RE_CompMesh.cpp
+++ RedEye/Source/RE_CompMesh.cpp
@@ -1,1 +1,148 @@
 #include "RE_CompMesh.h"
+
+#include "assimp/include/Importer.hpp"
+#include "assimp/include/scene.h"
+#include "assimp/include/postprocess.h"
+
+#pragma comment(lib, "assimp/libx86/assimp-vc140-mt.lib")
+
+#include "Application.h"
+#include "OutputLog.h"
+#include "FileSystem.h"
+#include "ModuleRenderer3D.h"
+#include "Texture2DManager.h"
+
+
+RE_CompMesh::RE_CompMesh(char * path)
+{
+	loadModel(path);
+}
+
+void RE_CompMesh::Draw(unsigned int shader)
+{
+	for (unsigned int i = 0; i < meshes.size(); i++)
+		meshes[i].Draw(shader);
+}
+
+void RE_CompMesh::loadModel(std::string path)
+{
+	RE_FileIO modelFBX(path.c_str());
+	
+	if (modelFBX.Load())
+	{
+		Assimp::Importer importer;
+		const aiScene *scene = importer.ReadFileFromMemory(modelFBX.GetBuffer(), modelFBX.GetSize(), aiProcess_Triangulate | aiProcess_FlipUVs);
+
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+		{
+			LOG("ERROR::ASSIMP::%s", importer.GetErrorString());
+			return;
+		}
+		directory = path.substr(0, path.find_last_of('/'));
+
+		processNode(scene->mRootNode, scene);
+	}
+}
+
+void RE_CompMesh::processNode(aiNode * node, const aiScene * scene)
+{
+	// process all the node's meshes (if any)
+	for (unsigned int i = 0; i < node->mNumMeshes; i++)
+	{
+		aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+		meshes.push_back(processMesh(mesh, scene));
+	}
+	// then do the same for each of its children
+	for (unsigned int i = 0; i < node->mNumChildren; i++)
+	{
+		processNode(node->mChildren[i], scene);
+	}
+}
+
+RE_Mesh RE_CompMesh::processMesh(aiMesh * mesh, const aiScene * scene)
+{
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+	{
+		// process vertex positions, normals and texture coordinates
+		Vertex vertex;
+		math::vec vector;
+		vector.x = mesh->mVertices[i].x;
+		vector.y = mesh->mVertices[i].y;
+		vector.z = mesh->mVertices[i].z;
+		vertex.Position = vector;
+
+		vector.x = mesh->mNormals[i].x;
+		vector.y = mesh->mNormals[i].y;
+		vector.z = mesh->mNormals[i].z;
+		vertex.Normal = vector;
+
+		if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
+		{
+			math::float2 vec;
+			vec.x = mesh->mTextureCoords[0][i].x;
+			vec.y = mesh->mTextureCoords[0][i].y;
+			vertex.TexCoords = vec;
+		}
+		else
+			vertex.TexCoords = math::float2(0.0f, 0.0f);
+
+		vertices.push_back(vertex);
+	}
+	// process indices
+	for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+	{
+		aiFace face = mesh->mFaces[i];
+		for (unsigned int j = 0; j < face.mNumIndices; j++)
+			indices.push_back(face.mIndices[j]);
+	}
+	// process material
+	if (mesh->mMaterialIndex >= 0)
+	{
+		if (mesh->mMaterialIndex >= 0)
+		{
+			aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
+			std::vector<Texture> diffuseMaps = loadMaterialTextures(material,
+				aiTextureType_DIFFUSE, "texture_diffuse");
+			textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+			std::vector<Texture> specularMaps = loadMaterialTextures(material,
+				aiTextureType_SPECULAR, "texture_specular");
+			textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+		}
+	}
+
+	return RE_Mesh(vertices, indices, textures);
+}
+
+std::vector<Texture> RE_CompMesh::loadMaterialTextures(aiMaterial * mat, aiTextureType type, std::string typeName)
+{
+	std::vector<Texture> textures;
+	for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+	{
+		aiString str;
+		mat->GetTexture(type, i, &str);
+		bool skip = false;
+		for (unsigned int j = 0; j < textures_loaded.size(); j++)
+		{
+			if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+			{
+				textures.push_back(textures_loaded[j]);
+				skip = true;
+				break;
+			}
+		}
+		if (!skip)
+		{   // if texture hasn't been loaded already, load it
+			Texture texture;
+			texture.id = App->renderer3d->texture_manager->LoadTexture2D(directory.c_str(), str.C_Str());
+			texture.type = typeName;
+			texture.path = str.C_Str();
+			textures.push_back(texture);
+			textures_loaded.push_back(texture); // add to loaded textures
+		}
+	}
+	return textures;
+}
--- RedEye/Source/RE_CompMesh.h
+++ RedEye/Source/RE_CompMesh.h
@@ -1,7 +1,33 @@
 #ifndef __RE_COMPMESH_H__
 #define __RE_COMPMESH_H__
 
+
+#include "RE_Mesh.h"
+
+struct aiNode;
+struct aiScene;
+struct aiMesh;
+struct aiMaterial;
+enum aiTextureType;
+
 class RE_CompMesh
 {
+public:
+	/*  Functions   */
+	RE_CompMesh(char *path);
+
+	void Draw(unsigned int shader);
+private:
+	/*  Model Data  */
+	std::vector<RE_Mesh> meshes;
+	std::string directory;
+	std::vector<Texture> textures_loaded;
+
+	/*  Functions   */
+	void loadModel(std::string path);
+	void processNode(aiNode *node, const aiScene *scene);
+	RE_Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+	std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type,
+		std::string typeName);
 };
 #endif // !__RE_COMPMESH_H__
