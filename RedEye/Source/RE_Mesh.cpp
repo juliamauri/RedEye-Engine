37fe7074259296141aa05c5a9ab3c48bbe9d880a
Ruben Sardon <ec4who@hotmail.com>
2018-10-04 02:37:30 +0200

    Added RE_MeshContainer :black_joker:

--- RedEye/Source/RE_Mesh.cpp
+++ RedEye/Source/RE_Mesh.cpp
@@ -5,8 +5,10 @@
 #include "ShaderManager.h"
 #include "Texture2DManager.h"
 
-#include "Glew/include/glew.h"
-#include <gl/GL.h>
+#include "assimp\include\scene.h"
+
+#include "Glew\include\glew.h"
+#include <gl\GL.h>
 
 RE_Mesh::RE_Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
 {
@@ -108,4 +110,131 @@ void RE_Mesh::setupMesh()
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
 
 	glBindVertexArray(0);
+}
+
+RE_MeshContainer::RE_MeshContainer(const char* f, const char* d, bool dropped) : dropped(dropped)
+{
+	if (f != nullptr) file_name = f;
+	if (d != nullptr) directory = d;
+}
+
+RE_MeshContainer::~RE_MeshContainer()
+{
+	meshes.clear();
+	textures_loaded.clear();
+}
+
+void RE_MeshContainer::Draw(unsigned int shader)
+{
+	for (unsigned int i = 0; i < meshes.size(); i++)
+		meshes[i].Draw(shader);
+}
+
+void RE_MeshContainer::ProcessNode(aiNode * node, const aiScene * scene)
+{
+	unsigned int i = 0;
+
+	// process all the node's meshes (if any)
+	for (; i < node->mNumMeshes; i++)
+	{
+		aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+		meshes.push_back(ProcessMesh(mesh, scene));
+	}
+
+	// then do the same for each children
+	for (i = 0; i < node->mNumChildren; i++)
+		ProcessNode(node->mChildren[i], scene);
+}
+
+RE_Mesh RE_MeshContainer::ProcessMesh(aiMesh * mesh, const aiScene * scene)
+{
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+	{
+		// process vertex positions, normals and texture coordinates
+		Vertex vertex;
+		math::vec vector;
+		vector.x = mesh->mVertices[i].x;
+		vector.y = mesh->mVertices[i].y;
+		vector.z = mesh->mVertices[i].z;
+		vertex.Position = vector;
+
+		vector.x = mesh->mNormals[i].x;
+		vector.y = mesh->mNormals[i].y;
+		vector.z = mesh->mNormals[i].z;
+		vertex.Normal = vector;
+
+		if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
+		{
+			math::float2 vec;
+			vec.x = mesh->mTextureCoords[0][i].x;
+			vec.y = mesh->mTextureCoords[0][i].y;
+			vertex.TexCoords = vec;
+		}
+		else
+			vertex.TexCoords = math::float2(0.0f, 0.0f);
+
+		vertices.push_back(vertex);
+	}
+	// process indices
+	for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+	{
+		aiFace face = mesh->mFaces[i];
+		for (unsigned int j = 0; j < face.mNumIndices; j++)
+			indices.push_back(face.mIndices[j]);
+	}
+	// process material
+	if (mesh->mMaterialIndex >= 0)
+	{
+		if (mesh->mMaterialIndex >= 0)
+		{
+			aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
+			std::vector<Texture> diffuseMaps = LoadMaterialTextures(material,
+				aiTextureType_DIFFUSE, "texture_diffuse");
+			textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+			std::vector<Texture> specularMaps = LoadMaterialTextures(material,
+				aiTextureType_SPECULAR, "texture_specular");
+			textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+		}
+	}
+
+	return RE_Mesh(vertices, indices, textures);
+}
+
+std::vector<Texture> RE_MeshContainer::LoadMaterialTextures(aiMaterial * mat, aiTextureType type, std::string typeName)
+{
+	std::vector<Texture> textures;
+	for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+	{
+		aiString str;
+		mat->GetTexture(type, i, &str);
+		bool skip = false;
+		for (unsigned int j = 0; j < textures_loaded.size(); j++)
+		{
+			if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+			{
+				textures.push_back(textures_loaded[j]);
+				skip = true;
+				break;
+			}
+		}
+		if (!skip)
+		{   // if texture hasn't been loaded already, load it
+			Texture texture;
+			texture.id = App->textures->LoadTexture2D(directory.c_str(), str.C_Str(), dropped);
+			texture.type = typeName;
+			texture.path = str.C_Str();
+			textures.push_back(texture);
+			textures_loaded.push_back(texture); // add to loaded textures
+		}
+	}
+	return textures;
+}
+
+const char * RE_MeshContainer::GetFileName() const
+{
+	return file_name.c_str();
 }
