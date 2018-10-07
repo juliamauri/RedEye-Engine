9969f6771f0299ecc968cc320d78a4645e97b3bb
Ruben Sardon <ec4who@hotmail.com>
2018-10-08 01:36:36 +0200

    Trying to solve mesh bug :warning:

--- RedEye/Source/ModuleScene.cpp
+++ RedEye/Source/ModuleScene.cpp
@@ -357,7 +357,7 @@ void ModuleScene::DrawScene()
 	*/
 
 	//root->Draw();
-	//drop->Draw();
+	drop->Draw();
 }
 
 //INIT
--- RedEye/Source/RE_Mesh.cpp
+++ RedEye/Source/RE_Mesh.cpp
@@ -65,15 +65,21 @@ void RE_Mesh::Draw(unsigned int shader_ID)
 	}
 	
 		// draw mesh
-		glBindVertexArray(VAO);
+	glBindVertexArray(VAO);
 
-		if (useIndex)
-			glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-		else
-			glDrawArrays(GL_TRIANGLES,0,vertices.size());
+	if (useIndex)
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	}
+	else
+	{
+		glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+	}
 
 
-		glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 	
 	// always good practice to set everything back to defaults once configured.
 	glActiveTexture(GL_TEXTURE0);
@@ -111,6 +117,8 @@ void RE_Mesh::setupMesh()
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
 
