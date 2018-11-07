e36043f2b135f590b9a7c3b3f47240408b3e4cc5
Julià Mauri Costa <julimacowork@gmail.com>
2018-11-07 07:36:43 +0100

    MeshManager fills gameobject and reference every loaded mesh  :sob:

--- RedEye/Source/MeshManager.cpp
+++ RedEye/Source/MeshManager.cpp
@@ -1,10 +1,13 @@
 #include "MeshManager.h"
 
 #include "Application.h"
+#include "RE_GameObject.h"
 #include "FileSystem.h"
 #include "ShaderManager.h"
+#include "Texture2DManager.h"
 #include "OutputLog.h"
 #include "Globals.h"
+#include "RE_CompMesh.h"
 #include "RE_Mesh.h"
 
 #include "SDL2\include\SDL_assert.h"
@@ -14,7 +17,7 @@
 
 #pragma comment(lib, "assimp/libx86/assimp-vc140-mt.lib")
 
-MeshManager::MeshManager(const char* f) : ResourceManager(R_MESH), folderPath(f)
+MeshManager::MeshManager(const char* f) : folderPath(f)
 {}
 
 MeshManager::~MeshManager()
@@ -40,48 +43,86 @@ bool MeshManager::Init(const char* def_shader)
 	return ret;
 }
 
-unsigned int MeshManager::LoadMesh(const char * path, bool dropped)
+void MeshManager::LoadMeshOnGameObject(RE_GameObject* parent, const char * name, const bool dropped)
 {
-	unsigned int ret = GetLoadedMesh(path, dropped);
-
+	to_fill = parent;
+	from_drop = dropped;
+	std::string path;
+	unsigned int ret = GetLoadedMesh(name , dropped);
 	if (!ret)
 	{
 		RE_FileIO* mesh_file = nullptr;
-		RE_FileIO mesh_file_tmp(path);
-
 		if (dropped)
 		{
-			mesh_file = App->fs->QuickBufferFromPDPath(path);
+			path += name;
+			mesh_file = App->fs->QuickBufferFromPDPath(path.c_str());
+			if (!mesh_file)
+				LOG("Error when load mesh from dropped file:\n%s", path.c_str());
 		}
-			
-		else if (mesh_file_tmp.Load())
+		else
 		{
-			mesh_file = &mesh_file_tmp;
+			path += folderPath;
+			path += name;
+			mesh_file = new RE_FileIO(path.c_str());
+			if (!mesh_file->Load())
+			{
+				LOG("Error when load mesh file:\n%s", path.c_str());
+				DEL(mesh_file);
+			}
 		}
 
 		if (mesh_file != nullptr)
 		{
-			Assimp::Importer importer;
-			const aiScene *scene = importer.ReadFileFromMemory(mesh_file->GetBuffer(), mesh_file->GetSize(), aiProcess_Triangulate | aiProcess_FlipUVs);
+			std::string path_s(path.c_str());
+			unsigned int separator_pos = path_s.find_last_of(dropped ? '\\' : '/');
+			file = path_s.substr(separator_pos, path_s.size()).c_str();
+			directory = path_s.substr(0, separator_pos).c_str();
 
-			if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+
+			LOG("Loading Model from: %s", path.c_str());
+			ProcessModel(mesh_file->GetBuffer(), mesh_file->GetSize());
+
+
+
+			DEL(mesh_file);
+		}
+	}
+}
+
+unsigned int MeshManager::LoadMesh(const char * name, bool dropped)
+{
+	std::string path;
+	unsigned int ret = GetLoadedMesh(name, dropped);
+	if (!ret)
+	{
+		RE_FileIO* mesh_file = nullptr;
+		if (dropped)
+		{
+			path += name;
+			mesh_file = App->fs->QuickBufferFromPDPath(path.c_str());
+			if (!mesh_file)
+				LOG("Error when load mesh from dropped file:\n%s", path.c_str());
+		}
+		else
+		{
+			path += folderPath;
+			path += name;
+			mesh_file = new RE_FileIO(path.c_str());
+			if (!mesh_file->Load())
 			{
-				LOG("ERROR::ASSIMP::%s", importer.GetErrorString());
-			}
-			else
-			{
-				std::string path_s(path);
-				unsigned int separator_pos = path_s.find_last_of(dropped ? '\\' : '/');
-				RE_MeshContainer* mesh = new RE_MeshContainer(
-					path_s.substr(separator_pos, path_s.size()).c_str(),
-					path_s.substr(0, separator_pos).c_str(),
-					true);
-				mesh->ProcessNode(scene->mRootNode, scene);
-				ret = AddMesh(mesh);
+				LOG("Error when load mesh file:\n%s", path.c_str());
+				DEL(mesh_file);
 			}
 		}
 
-		if (dropped) DEL(mesh_file);
+		if (mesh_file != nullptr)
+		{
+			LOG("Loading Model from: %s", path.c_str());
+
+			//ProcesMesh(mesh_file->GetBuffer(), mesh_file->GetSize());
+
+			DEL(mesh_file);
+		}
 	}
 
 	return ret;
@@ -96,18 +137,161 @@ unsigned int MeshManager::GetLoadedMesh(const char * path, const bool from_drop)
 void MeshManager::DrawMesh(const unsigned int reference)
 {
 	if (reference > 0)
-		((RE_MeshContainer*)resources.at(reference).first)->Draw(default_shader);
+		((RE_Mesh*)App->resources->At(reference))->Draw(default_shader);
 }
 
-unsigned int MeshManager::AddMesh(RE_MeshContainer * mesh)
+unsigned int MeshManager::AddMesh(RE_Mesh * mesh)
 {
-	unsigned int ret = 1 + (resources.size() * 2);
-	ResourceIter it = resources.find(ret);
+	return App->resources->Reference((ResourceContainer*)mesh);
+}
 
-	while (it != resources.end())
-		it = resources.find(ret--);
+void MeshManager::ProcessModel(const char* buffer, unsigned int size)
+{
+	Assimp::Importer importer;
+	const aiScene *scene = importer.ReadFileFromMemory(buffer, size, aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_SortByPType);;
+	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+		LOG_ERROR("ASSIMP couldn't import file from memory! Assimp error: %s", importer.GetErrorString());
+	else
+	{
+		ProcessNode(scene->mRootNode, scene);
+		
 
-	resources[ret] = { (ResourceContainer*)mesh, 1 };
-	
-	return ret;
+		/*
+
+			*/
+		//ret = AddMesh(mesh_container);
+	}
+
+
+}
+
+void MeshManager::ProcessNode(aiNode * node, const aiScene * scene)
+{
+	RE_GameObject* go = new RE_GameObject(node->mName.C_Str(), to_fill);
+	//RE_CompMesh* compmesh = new RE_CompMesh;
+	//mesh->AddComponent((RE_Component*))
+
+	LOG_SECONDARY("%s Node: %s (%u meshes | %u children)",
+		node->mParent ? "SON" : "PARENT",
+		node->mName.C_Str(),
+		node->mNumMeshes,
+		node->mNumChildren);
+
+	unsigned int i = 0;
+
+	for (; i < node->mNumMeshes; i++)
+	{
+		aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+		RE_CompMesh* comp_mesh = new RE_CompMesh(go, App->resources->Reference((ResourceContainer*)ProcessMesh(mesh, scene, i + 1)));
+		go->AddCompMesh(comp_mesh);
+		//meshes.rbegin()->name = node->mName.C_Str();
+		//total_triangle_count += meshes.rbegin()->triangle_count;
+	}
+
+	for (i = 0; i < node->mNumChildren; i++)
+	{
+		to_fill = go;
+		ProcessNode(node->mChildren[i], scene);
+	}
+}
+
+RE_Mesh* MeshManager::ProcessMesh(aiMesh * mesh, const aiScene * scene, const unsigned int pos)
+{
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	LOG_TERCIARY("Mesh %u: %s (%u vertices | %u faces | %u texture indexes | %u bones)",
+		pos,
+		mesh->mName.C_Str(),
+		mesh->mNumVertices,
+		mesh->mNumFaces,
+		mesh->mMaterialIndex,
+		mesh->mNumBones);
+
+	// process vertices
+	for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+	{
+		// process vertex positions, normals and texture coordinates
+		
+		Vertex vertex;
+		/*f ((*/vertex.Position.x = mesh->mVertices[i].x;//) < bounding_box.minPoint.x) bounding_box.minPoint.x = vertex.Position.x;
+		/*else if ((*/vertex.Position.x = mesh->mVertices[i].x;//) > bounding_box.maxPoint.x) bounding_box.maxPoint.x = vertex.Position.x;
+		/*if ((*/vertex.Position.y = mesh->mVertices[i].y;//) < bounding_box.minPoint.y) bounding_box.minPoint.y = vertex.Position.y;
+		/*else if ((*/vertex.Position.y = mesh->mVertices[i].y;//) > bounding_box.maxPoint.y) bounding_box.maxPoint.y = vertex.Position.y;
+		/*if ((*/vertex.Position.z = mesh->mVertices[i].z;//) < bounding_box.minPoint.z) bounding_box.minPoint.z = vertex.Position.z;
+		/*else if ((*/vertex.Position.z = mesh->mVertices[i].z;//) > bounding_box.maxPoint.z) bounding_box.maxPoint.z = vertex.Position.x;
+
+		vertex.Normal.x = mesh->mNormals[i].x;
+		vertex.Normal.y = mesh->mNormals[i].y;
+		vertex.Normal.z = mesh->mNormals[i].z;
+
+		if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
+		{
+			vertex.TexCoords.x = mesh->mTextureCoords[0][i].x;
+			vertex.TexCoords.y = mesh->mTextureCoords[0][i].y;
+		}
+		else
+			vertex.TexCoords = math::float2::zero;
+
+		vertices.push_back(vertex);
+	}
+
+	// process indices
+	for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+	{
+		aiFace face = mesh->mFaces[i];
+
+		if (face.mNumIndices != 3)
+		{
+			error_loading = true;
+			LOG_WARNING("Loading geometry face with %u indexes (instead of 3)", face.mNumIndices);
+		}
+
+		for (unsigned int j = 0; j < face.mNumIndices; j++)
+			indices.push_back(face.mIndices[j]);
+	}
+
+	// process material
+	aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
+	std::vector<Texture> diffuseMaps = LoadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+	textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+	//std::vector<_Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+	//textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+
+	RE_Mesh* ret_mesh = new RE_Mesh(vertices, indices, textures, mesh->mNumFaces);
+	ResourceContainer* mesh_resource = (ResourceContainer*)ret_mesh;
+	mesh_resource->SetType(Resource_Type::R_MESH);
+
+	return ret_mesh;
+}
+
+std::vector<Texture> MeshManager::LoadMaterialTextures(aiMaterial * mat, aiTextureType type, std::string typeName)
+{
+	std::vector<Texture> textures;
+	for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+	{
+		aiString str;
+		mat->GetTexture(type, i, &str);
+		bool skip = false;
+		for (unsigned int j = 0; j < textures_loaded.size(); j++)
+		{
+			if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+			{
+				textures.push_back(textures_loaded[j]);
+				skip = true;
+				break;
+			}
+		}
+		if (!skip)
+		{   // if texture hasn't been loaded already, load it
+			Texture texture;
+			texture.id = App->textures->LoadTexture2D(directory.c_str(), str.C_Str(), from_drop);
+			texture.type = typeName;
+			texture.path = str.C_Str();
+			textures.push_back(texture);
+			textures_loaded.push_back(texture); // add to loaded textures
+		}
+	}
+	return textures;
 }
--- RedEye/Source/MeshManager.h
+++ RedEye/Source/MeshManager.h
@@ -2,28 +2,50 @@
 #define __MESHMANAGER_H__
 
 #include "ResourceManager.h"
+#include <vector>
 
 class RE_MeshContainer;
+class RE_GameObject;
+class RE_Mesh;
+struct aiNode;
+struct aiMesh;
+struct aiScene;
+struct aiMaterial;
+enum aiTextureType;
+struct Texture;
 
-class MeshManager : ResourceManager
+class MeshManager
 {
 public:
 	MeshManager(const char* folderPath = nullptr);
 	~MeshManager();
 
 	bool Init(const char* def_shader = nullptr);
+	void LoadMeshOnGameObject(RE_GameObject* parent, const char* path, const bool dropped = false);
 	unsigned int LoadMesh(const char* path, const bool dropped = false);
 	unsigned int GetLoadedMesh(const char* path, const bool from_drop = false) const;
 	void DrawMesh(const unsigned int reference);
 
 private:
 
-	unsigned int AddMesh(RE_MeshContainer* mesh);
-	
+	unsigned int AddMesh(RE_Mesh* mesh);
+	void ProcessModel(const char* buffer, unsigned int size);
+	void ProcessNode(aiNode* node, const aiScene* scene);
+	RE_Mesh* ProcessMesh(aiMesh* mesh, const aiScene* scene, const unsigned int pos);
+	std::vector<Texture> LoadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+
 private:
 
 	const char* folderPath = nullptr;
 	unsigned int default_shader = 0;
+
+	RE_GameObject* to_fill = nullptr;
+	bool from_drop = false;
+	bool error_loading = false;
+	std::vector<RE_Mesh*> meshes_to_fill;
+	std::vector<Texture> textures_loaded;
+	std::string file;
+	std::string directory;
 };
 
 #endif // !__MESHMANAGER_H__
