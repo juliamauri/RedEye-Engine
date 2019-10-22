e7243ff6d37334a958135d1c24397ad7a1d86d62
Ruben Sardon <ec4who@hotmail.com>
2019-10-22 03:01:05 +0200

    Reduced log max length :crocodile:

--- RedEye/Source/OutputLog.cpp
+++ RedEye/Source/OutputLog.cpp
@@ -6,32 +6,32 @@
 #include <stdio.h>
 
 #define WIN32_MEAN_AND_LEAN
-#define LOG_STATEMENT_MAX_LENGTH 4096
+#define LOG_STATEMENT_MAX_LENGTH 512
 
 void _log(const int category, const char file[], int line, const char* format, ...)
 {
-	static char tmp_string[4096];
-	static char tmp_string2[4096];
+	static char tmp_string[LOG_STATEMENT_MAX_LENGTH];
+	static char tmp_string2[LOG_STATEMENT_MAX_LENGTH];
 	static va_list  ap;
 
 	// Construct the string from variable arguments
 	va_start(ap, format);
-	vsprintf_s(tmp_string, 4096, format, ap);
+	vsprintf_s(tmp_string, LOG_STATEMENT_MAX_LENGTH, format, ap);
 	va_end(ap);
-	sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
+	sprintf_s(tmp_string2, LOG_STATEMENT_MAX_LENGTH, "\n%s(%d) : %s", file, line, tmp_string);
 	OutputDebugString(tmp_string2);
 
 	if (App != nullptr)
 	{
 		switch (LogCategory(category))
 		{
-		case L_SEPARATOR: sprintf_s(tmp_string2, 4096, "\n\n=============\t%s\t=============", tmp_string); break;
-		case L_GLOBAL: sprintf_s(tmp_string2, 4096, "\n%s", tmp_string); break;
-		case L_SECONDARY: sprintf_s(tmp_string2, 4096, "\n\t- %s", tmp_string); break;
-		case L_TERCIARY: sprintf_s(tmp_string2, 4096, "\n\t\t+ %s", tmp_string); break;
-		case L_ERROR: sprintf_s(tmp_string2, 4096, "\nERROR: %s", tmp_string); break;
-		case L_WARNING: sprintf_s(tmp_string2, 4096, "\nWARNING: %s", tmp_string); break;
-		case L_SOFTWARE: sprintf_s(tmp_string2, 4096, "\n\t* 3rd party software report: %s", tmp_string); break;
+		case L_SEPARATOR: sprintf_s(tmp_string2, LOG_STATEMENT_MAX_LENGTH, "\n\n=============\t%s\t=============", tmp_string); break;
+		case L_GLOBAL: sprintf_s(tmp_string2, LOG_STATEMENT_MAX_LENGTH, "\n%s", tmp_string); break;
+		case L_SECONDARY: sprintf_s(tmp_string2, LOG_STATEMENT_MAX_LENGTH, "\n\t- %s", tmp_string); break;
+		case L_TERCIARY: sprintf_s(tmp_string2, LOG_STATEMENT_MAX_LENGTH, "\n\t\t+ %s", tmp_string); break;
+		case L_ERROR: sprintf_s(tmp_string2, LOG_STATEMENT_MAX_LENGTH, "\nERROR: %s", tmp_string); break;
+		case L_WARNING: sprintf_s(tmp_string2, LOG_STATEMENT_MAX_LENGTH, "\nWARNING: %s", tmp_string); break;
+		case L_SOFTWARE: sprintf_s(tmp_string2, LOG_STATEMENT_MAX_LENGTH, "\n\t* 3rd party software report: %s", tmp_string); break;
 		}
 		
 		App->Log(category, tmp_string2, file);
