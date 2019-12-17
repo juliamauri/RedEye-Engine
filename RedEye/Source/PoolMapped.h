aa770d2da7cd6d937f2932891995a79a62864887
Julià Mauri Costa <julimacowork@gmail.com>
2019-12-17 23:29:01 +0100

    template abstract class PoolMapped :b:

---
+++ RedEye/Source/PoolMapped.h
@@ -0,0 +1,51 @@
+#ifndef __POOL_H__
+#define __POOL_H__
+
+#include <vector>
+#include <map>
+
+#define MAX_POOL 1024
+
+template<class TYPEVALUE, class TYPEKEY>
+class PoolMapped
+{
+public:
+	PoolMapped() {
+		pool_ = new TYPEVALUE[MAX_POOL];
+	}
+	virtual ~PoolMapped() {
+		DEL_A(pool_);
+	}
+
+	virtual TYPEKEY Push(TYPEVALUE val) = 0;
+
+	virtual void Push(TYPEVALUE val, TYPEKEY key) {
+		pool_[lastAvaibleIndex] = val;
+		poolmapped_.insert(std::pair<TYPEKEY, unsigned int>(key, lastAvaibleIndex++));
+	}
+
+	virtual TYPEVALUE Pop(TYPEKEY key) {
+		std::map<TYPEKEY, unsigned int>::iterator w = poolmapped_.find(key);
+		unsigned int index = w->second;
+		TYPEVALUE ret = pool_[index];
+		memcpy(pool_ + index, pool_ + lastAvaibleIndex, sizeof(TYPEVALUE*) * (lastAvaibleIndex - index));
+		while (w != poolmapped_.end())
+		{
+			w->second--;
+			w++;
+		}
+		lastAvaibleIndex--;
+
+		return ret;
+	}
+
+	RE_GameObject* At(UUID key)const { return pool_[poolmapped_.at(key)]; }
+
+protected:
+	TYPEVALUE* pool_;
+	std::map<TYPEKEY, unsigned int> poolmapped_;
+
+	unsigned int lastAvaibleIndex = 0;
+};
+
+#endif // !__POOL_H__
