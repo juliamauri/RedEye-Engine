4b77c5e357db567358469f0d61cf07a45b0380e1
Julià Mauri Costa <julimacowork@gmail.com>
2024-12-21 00:09:37 +0100

    [Project] Remove RedEye_old

--- RedEye_old/Engine/RE_Assert.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RE_ASSERT__
-#define __RE_ASSERT__
-
-// Define Assert to call EA_ASSERT
-#include <EAAssert/eaassert.h>
-#define RE_ASSERT(expression) EA_ASSERT(expression)
-
-#endif // !__RE_ASSERT__
